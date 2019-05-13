8e4636732de61d2d77b2c2a5a8a3f405dc26a6d2
s3543536 <s3543536@student.rmit.edu.au>
2019-05-13 16:48:38 +1000

    added iterator erase operations for vertices

--- src/mesh/mesh.cpp
+++ src/mesh/mesh.cpp
@@ -1,4 +1,57 @@
 #include "mesh.h"
 
+
 mesh::mesh(void): vertices() {}
 
+void mesh::test(void) {
+	std::set<struct vertex>::iterator asdf = vertices.begin();
+}
+
+
+
+// get vertex iterator
+mesh::vert_cit mesh::cbegin_vertex() const noexcept {
+	return vertices.cbegin();
+}
+mesh::vert_cit mesh::cend_vertex() const noexcept {
+	return vertices.cend();
+}
+
+// erase vertex
+mesh::vert_cit mesh::erase_vertex(vert_cit pos) {
+	return vertices.erase(pos);
+}
+
+mesh::vert_size_type mesh::erase_vertex(const struct vertex& pos) {
+	return vertices.erase(pos);
+}
+
+void mesh::erase_vertex(vert_cit first, vert_cit last) {
+	vertices.erase(first, last);
+}
+
+// mark erase vertex
+void mesh::mark_erase_vertex(vert_cit pos) {
+	pos->to_delete = true;
+}
+
+void mesh::mark_erase_vertex(const struct vertex& pos) {
+	pos.to_delete = true;
+}
+
+void mesh::mark_erase_vertex(vert_cit first, vert_cit last) {
+	for(vert_cit it = first; it != last; it++) {
+		it->to_delete = true;
+	}
+}
+
+void mesh::erase_tris_with_deleted_vertices() {
+	for(auto it = tris.begin(); it != tris.end();) {
+		if( it->vertices[0]->to_delete ||
+				it->vertices[1]->to_delete ||
+				it->vertices[2]->to_delete )
+			it = tris.erase(it);
+		else
+			it++;
+	}
+}
--- src/mesh/mesh.h
+++ src/mesh/mesh.h
@@ -26,6 +26,20 @@ class mesh {
 		friend bool operator==(const vertex& left, const vertex& right) {
 			return !(left < right) && !(right < left);
 		}
+
+		private:
+		  // want to mark vertices for deletion before they are deleted.
+			//   this is because any tri could use this vertex, so to avoid
+			//   O(n^2) behavior, we mark the vertices to be deleted, then
+			//   we check each tri, then we delete the vertex
+			// this can not be part of the key (compare func), otherwise 
+			//   we would not be able to change the value while the set is
+			//   holding it, since the key affects the position of the item,
+			//   and the set does not change position of the item while it
+			//   is in the set.
+			//   (which is a feature of set we are making deliberate use of)
+			mutable bool to_delete;
+			friend class mesh;
 	};
 
 	struct triangle {
@@ -48,15 +62,44 @@ class mesh {
 		}
 	};
 	// http://eel.is/c++draft/associative.reqmts#9
-	// "The insert and emplace members shall not affect the validity of iterators 
-	//   and references to the container, and the erase members shall invalidate 
-	//   only iterators and references to the erased elements."
-	// this means, the tris can hold a const reference to the vertices, without
-	//   the references being invalidated
+	// "The insert and emplace members shall not affect the validity of 
+	//   iterators and references to the container, and the erase members 
+	//   shall invalidate only iterators and references to the erased 
+	//   elements."
+	// this means, the tris can hold a const reference to the vertices, 
+	//   without the references being invalidated
 	std::set<struct vertex> vertices;
 	std::set<struct triangle> tris;
 
 	public:
 		mesh(void);
 
+		void test(void);
+
+		using vert_cit = std::set<struct vertex>::const_iterator;
+		using vert_size_type = std::set<struct vertex>::size_type;
+
+		// NYI
+		std::pair<vert_cit,bool> insert_vertex( const struct vertex& value );
+		void insert_vertex(std::initializer_list<struct vertex> ilist);
+
+		vert_cit cbegin_vertex() const noexcept;
+		vert_cit cend_vertex() const noexcept;
+
+		// these are dangerous
+		//   when you erase a vertex, any triangles with references
+		//   to that vertex will be invalidated. you need to mark the
+		//   vertex to be erased, then call:
+		//     erase_tris_with_deleted_vertices()
+		//   then erase the vertex
+		// the safe way to do this is order O(n^2) as opposed to O(n)
+		vert_cit erase_vertex(vert_cit pos);
+		vert_size_type erase_vertex(const struct vertex& key);
+		void erase_vertex(vert_cit first, vert_cit last);
+
+		void mark_erase_vertex(vert_cit pos);
+		void mark_erase_vertex(const struct vertex& key);
+		void mark_erase_vertex(vert_cit first, vert_cit last);
+
+		void erase_tris_with_deleted_vertices();
 };
