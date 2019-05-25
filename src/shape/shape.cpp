43a90b2dcf64570b570863b49337b91f02399e29
s3543536 <s3543536@student.rmit.edu.au>
2019-05-25 16:10:33 +1000

    fixed bug where everything would go dark

--- src/shape/shape.cpp
+++ src/shape/shape.cpp
@@ -3,14 +3,17 @@
 
 void Shape::draw(const DrawOptions &options) const {
   // push settings
+  glPushAttrib(GL_ENABLE_BIT);
   glPushAttrib(GL_NORMALIZE);
   glPushAttrib(GL_COLOR_MATERIAL);
+
   glEnable(GL_NORMALIZE);
   if(is_material_active)
     glMaterialfv(GL_FRONT_AND_BACK, material);
 
   really_draw(options);
 
+  glPopAttrib();
   glPopAttrib();
   glPopAttrib();
 }
