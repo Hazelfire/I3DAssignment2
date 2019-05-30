2e0de349c030d4e745cb0804065a07cd64ad3708
s3543536 <s3543536@student.rmit.edu.au>
2019-05-30 17:26:44 +1000

    added ctors

--- src/gameobject/animated_gameobject/animated_gameobject.cpp
+++ src/gameobject/animated_gameobject/animated_gameobject.cpp
@@ -1,5 +1,28 @@
 #include "animated_gameobject.hpp"
 
+
+template <typename anim_id>
+animated_gameobject<anim_id>::animated_gameobject():
+      animated_gameobject(nullptr, nullptr) {};
+template <typename anim_id>
+animated_gameobject<anim_id>::animated_gameobject(std::shared_ptr<Shape> shape):
+      animated_gameobject(shape, nullptr) {};
+template <typename anim_id>
+animated_gameobject<anim_id>::animated_gameobject(std::shared_ptr<Shape> shape, std::shared_ptr<Shape> collider):
+      GameObject(shape, collider), playing(nullptr) {};
+template <typename anim_id>
+
+
+animated_gameobject<anim_id>::animated_gameobject(std::unique_ptr<std::set<animation>> &&anims):
+      animated_gameobject(nullptr, nullptr, anims) {};
+template <typename anim_id>
+animated_gameobject<anim_id>::animated_gameobject(std::shared_ptr<Shape> shape, std::unique_ptr<std::set<animation>> &&anims):
+      animated_gameobject(shape, nullptr, anims) {};
+template <typename anim_id>
+animated_gameobject<anim_id>::animated_gameobject(std::shared_ptr<Shape> shape, std::shared_ptr<Shape> collider, std::unique_ptr<std::set<animation>> &&anims):
+      GameObject(shape, collider), animations(*anims.release()) {};
+
+
 template <typename anim_id>
 bool animated_gameobject<anim_id>::animation::operator<(const animation& r) const {
   return name < r.name;
@@ -14,3 +37,20 @@ animated_gameobject<anim_id>::animation::keyframe::keyframe(
 {
 }
 
+/*
+void animated_gameobject::draw(DrawOptions ops){
+  this->pushRelativeTransform();
+
+  if(shape)
+    shape->draw(ops);
+
+  for(auto child : children) {
+    child->draw(ops);
+  }
+
+  this->popRelativeTransform();
+  if(ops.collider && collider){
+    collider->draw(ops);
+  }
+}
+*/
--- src/gameobject/animated_gameobject/animated_gameobject.hpp
+++ src/gameobject/animated_gameobject/animated_gameobject.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "gameobject/gameobject.hpp"
 #include <vector>
 
@@ -12,6 +13,11 @@ class animated_gameobject : public GameObject {
         keyframe(const v3d &position, const v3d &rotation, double time_offset);
       };
 
+      private:
+      double total_len;
+
+      public:
+      const double& length = total_len;
       anim_id name;
       std::vector<keyframe> frames;
 
@@ -23,7 +29,15 @@ class animated_gameobject : public GameObject {
   private:
     v3d orig_position;
     v3d orig_rotation;
+    animation *playing;
 
   public:
+    animated_gameobject();
+    animated_gameobject(std::shared_ptr<Shape> shape);
+    animated_gameobject(std::shared_ptr<Shape> shape, std::shared_ptr<Shape> collider);
+    animated_gameobject(std::unique_ptr<std::set<animation>> &&anims);
+    animated_gameobject(std::shared_ptr<Shape> shape, std::unique_ptr<std::set<animation>> &&anims);
+    animated_gameobject(std::shared_ptr<Shape> shape, std::shared_ptr<Shape> collider, std::unique_ptr<std::set<animation>> &&anims);
+
     virtual void draw(DrawOptions ops) override;
 };
